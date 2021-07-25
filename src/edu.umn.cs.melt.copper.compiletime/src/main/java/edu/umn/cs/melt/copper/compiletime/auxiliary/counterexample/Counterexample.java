c7b07a310d5cad5b48c86b7bbc5a4238b98795f7
kelton <kelton@keltono.net>
2021-07-25 03:50:35 -0500

    fix coloured counterexamples

--- src/edu.umn.cs.melt.copper.compiletime/src/main/java/edu/umn/cs/melt/copper/compiletime/auxiliary/counterexample/Counterexample.java
+++ src/edu.umn.cs.melt.copper.compiletime/src/main/java/edu/umn/cs/melt/copper/compiletime/auxiliary/counterexample/Counterexample.java
@@ -6,15 +6,15 @@ public class Counterexample {
     private Derivation derivation1;
     private Derivation derivation2;
     private boolean isShiftReduce;
-    private ArrayList<StringBuilder> sb1 = new ArrayList<>();
-    private ArrayList<StringBuilder> sb2 = new ArrayList<>();
+    private ArrayList<ColouredStringBuilder> sb1 = new ArrayList<>();
+    private ArrayList<ColouredStringBuilder> sb2 = new ArrayList<>();
 
     public Counterexample(Derivation derivation1, Derivation derivation2, boolean isShiftReduce) {
         this.derivation1 = derivation1;
         this.derivation2 = derivation2;
         this.isShiftReduce = isShiftReduce;
-        sb1.add(new StringBuilder());
-        sb2.add(new StringBuilder());
+        sb1.add(new ColouredStringBuilder());
+        sb2.add(new ColouredStringBuilder());
     }
 
     //TODO
@@ -22,20 +22,16 @@ public class Counterexample {
         StringBuilder sb = new StringBuilder();
         sb.append("Example:\n");
         sb.append(isShiftReduce? "shift derivation:\n" : "first reduce derivation:\n");
-        ArrayList<Integer> escapes = new ArrayList<>();
-        escapes.add(0);
-        derivation1.prettyPrint(sb1,escapes,0,0);
-        for(StringBuilder s : sb1){
+        derivation1.prettyPrint(sb1,0,0);
+        for(ColouredStringBuilder s : sb1){
             sb.append(s);
-            sb.append('\n'+Derivation.ANSI_RESET);
+            sb.append('\n');
         }
         sb.append(isShiftReduce? "reduce derivation:\n" : "second reduce derivation:\n");
-        escapes = new ArrayList<>();
-        escapes.add(0);
-        derivation2.prettyPrint(sb2,escapes,0,0);
-        for(StringBuilder s : sb2){
+        derivation2.prettyPrint(sb2,0,0);
+        for(ColouredStringBuilder s : sb2){
             sb.append(s);
-            sb.append('\n'+Derivation.ANSI_RESET);
+            sb.append('\n');
         }
         return sb.toString();
     }
--- src/edu.umn.cs.melt.copper.compiletime/src/main/java/edu/umn/cs/melt/copper/compiletime/auxiliary/counterexample/Derivation.java
+++ src/edu.umn.cs.melt.copper.compiletime/src/main/java/edu/umn/cs/melt/copper/compiletime/auxiliary/counterexample/Derivation.java
@@ -2,9 +2,9 @@ package edu.umn.cs.melt.copper.compiletime.auxiliary.counterexample;
 
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
+import static edu.umn.cs.melt.copper.compiletime.auxiliary.counterexample.ColouredStringBuilder.*;
 
 /**
  * A derivation is a parse tree, represented by a symbol (which could a terminal or a non-terminal),
@@ -16,24 +16,6 @@ public class Derivation {
 
     public static final Derivation dot = new Derivation("(*)", null);
 
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String[] ansiColours = {"\u001B[36m","\u001B[33m","\u001B[32m","\u001B[34m","\u001B[31m","\u001B[35m"};
-    private static int currentColour = 0;
-
-    private static void incrementCurrentColour(){
-        if(currentColour + 1 == ansiColours.length){
-            currentColour = 0;
-        } else {
-            currentColour++;
-        }
-    }
-    private static void decrementCurrentColour(){
-        if(currentColour - 1 < 0){
-            currentColour = ansiColours.length - 1;
-        } else {
-            currentColour--;
-        }
-    }
 
     public Derivation(String symbol){
         this.symbol = symbol;
@@ -46,18 +28,14 @@ public class Derivation {
 
 
     //return value the new indent
-    public int prettyPrint(ArrayList<StringBuilder> sbs,ArrayList<Integer> escapeCounts,int index,int indent) {
+    public int prettyPrint(ArrayList<ColouredStringBuilder> sbs, int index,int indent) {
         //print LHS/terminal
-        StringBuilder sb = sbs.get(index);
-        Integer sbEscapes = escapeCounts.get(index);
+        ColouredStringBuilder sb = sbs.get(index);
         if(derivations != null){
             incrementCurrentColour();
         }
         sb.append(" ");
-        sb.append(ansiColours[currentColour]);
-        sb.append(symbol);
-        sb.append(ANSI_RESET);
-        escapeCounts.set(index,escapeCounts.get(index) + 2);
+        sb.appendColoured(symbol);
 
         indent += symbol.length() + 1;
         if(derivations == null){
@@ -68,25 +46,22 @@ public class Derivation {
         try {
             sbs.get(index+1);
         } catch(IndexOutOfBoundsException e) {
-           sbs.add(new StringBuilder());
-           escapeCounts.add(0);
+           sbs.add(new ColouredStringBuilder());
         }
 
-        StringBuilder nextsb = sbs.get(index+1);
-        for (int i = nextsb.length()-escapeCounts.get(index+1); i < indent-1; i++) {
+        ColouredStringBuilder nextsb = sbs.get(index+1);
+        for (int i = nextsb.length(); i < indent-1; i++) {
             nextsb.append(" ");
         }
-        nextsb.append(ansiColours[currentColour] + "↳" + ANSI_RESET);
-        escapeCounts.set(index+1,escapeCounts.get(index+1)+2);
-//        nextsb.append("↳");
+        nextsb.appendColoured("↳");
 
         //print rhs
         for(Derivation d : derivations){
-           indent = d.prettyPrint(sbs,escapeCounts,index+1,indent);
+           indent = d.prettyPrint(sbs,index+1,indent);
         }
 
         //account for newly required whitespace
-        for (int i = sb.length()-escapeCounts.get(index); i < indent; i++) {
+        for (int i = sb.length(); i < indent; i++) {
             sb.append(" ");
         }
         decrementCurrentColour();
